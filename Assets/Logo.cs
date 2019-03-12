853411772a2244c3931330ecef7aabcd67148d6a
Mikhael Dionio <mikhaeldionio@gmail.com>
2019-03-12 23:20:08 +0800

    add goal room linked to logo

---
+++ Assets/Logo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logo: MonoBehaviour {
+    public float rotateSpeed;
+    public GameObject endGameLocation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0.0f, rotateSpeed * Time.deltaTime, 0.0f);
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("You did it!!");
+            collision.transform.position = endGameLocation.transform.position;
+        }
+    }
+}
